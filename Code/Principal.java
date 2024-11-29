7d7740ff7c0fb51cdae0fcfb88389edaa151c38f
simaovsky <william_s94@hotmail.com>
2024-11-29 18:13:10 -0300

    adicionando classe login

--- Code/Principal.java
+++ Code/Principal.java
@@ -1,5 +1,8 @@
 public class Principal{
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
+        Login login = new Login("teste@teste.com", "");
+
+        System.out.println(login.getEmail());
     }
 }
